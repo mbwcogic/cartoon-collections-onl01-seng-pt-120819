91189589708ac0f50b969e04435ca615b339985b
Michael Waldo <mbwcogic@yahoo.com>
2019-12-29 15:15:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(array)# code an argument here
 end
 end
 def summon_captain_planet(planeteer_calls)# code an argument here
-  # Your code here
+  planeteer_calls.map # Your code here
 end
 
 def long_planeteer_calls# code an argument here
