024a2325030cc1ec56c563dbc4e159bc7438d7a6
Michael Waldo <mbwcogic@yahoo.com>
2019-12-29 15:26:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 def long_planeteer_calls(array)
-  array.any?{|i| i.length < 4}
+  array.any? { |calls| calls.length < 4}
 end 
   # code an argument here
   # Your code here
