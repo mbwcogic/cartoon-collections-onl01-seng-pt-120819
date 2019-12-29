d80830d37356821e4f4892e4064a968b20cc53bf
Michael Waldo <mbwcogic@yahoo.com>
2019-12-29 15:04:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)# code an argument here
- array.each_with_index do |array|, |index| 
+ array.each_with_index do |array, index| 
 end
 
 def summon_captain_planet# code an argument here
