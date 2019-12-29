b3fc250fe936621f6131c3144118c4caaca873df
Michael Waldo <mbwcogic@yahoo.com>
2019-12-29 15:24:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 def long_planeteer_calls(array)
-  array.any?
+  array.any?{|i| i > 3}
+end
   
   # code an argument here
   # Your code here
