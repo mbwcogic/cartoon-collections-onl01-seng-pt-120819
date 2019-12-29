66c7a89f3f9b2b1ab04e1ffb6b781a99c9a8c981
Michael Waldo <mbwcogic@yahoo.com>
2019-12-29 15:21:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,10 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.map {|x| x.capitalize + "!"} # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
+  
+  
+  # code an argument here
   # Your code here
 end
 
